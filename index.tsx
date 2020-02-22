f4d903f6edc25cdca8391bf644f0f3a34360910c
MorenoJoshua <joshua200128@gmail.com>
2020-02-22 11:56:00 -0800

    1 file modified

--- index.tsx
+++ index.tsx
@@ -10,8 +10,17 @@ class App extends Component<AppProps, AppState> {
   render() {
     return (
       <div>
-        <Section id="header" className="bg-danger" center>
-          header
+        <Section id="header" className="bg-danger" center wide>
+          Bienvenido a ABBC
+        </Section>
+        <Section id="quienesSomos" className="" center>
+          Quienes somos
+        </Section>
+        <Section id="baristaBattleJumbotron" className="bg-danger" wide>
+        <div className="container text-right">Barista Battle</div>
+        </Section>
+        <Section id="talleres" className="" center>
+          Talleres
         </Section>
       </div>
     );
