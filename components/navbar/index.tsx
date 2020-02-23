61c245ec31339732f09632d6fd6707152ef214a8
MorenoJoshua <joshua200128@gmail.com>
2020-02-23 13:50:23 -0800

    1 file modified

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./style.scss";
-
+import { assetUrl } from "../../helpers";
 // TODO: Animacion
 type NavbarProps = {};
 class Navbar extends React.Component<NavbarProps> {
   render() {
     return (
-      <div className="navbar bg-coffee">
+      <div className="navbar">
         <div className="container">
-          <div>Logo</div>
           <div>
+            <img className="navbar-image" src={assetUrl("ABBCWhite-1.png")} />
+          </div>
+          <div className="d-flex align-items-start" style={{h}}>
             <span className="navbar-button">Conocenos</span>
             <span className="navbar-button">Blog</span>
             <span className="navbar-button">Talleres</span>
